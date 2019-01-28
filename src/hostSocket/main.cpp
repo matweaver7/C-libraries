d723223078e82d48755498bce0bd7bd3052d2230
Mat Weaver <mat.weaver@purple.com>
2019-01-27 19:42:41 -0700

    added server socket stuff, and bare bones of multithreading

--- src/hostSocket/main.cpp
+++ src/hostSocket/main.cpp
@@ -2,22 +2,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 using namespace std;
-struct sockadd_in6 {
-    sa_family_t     sin6_family;   /* AF_INET6 */
-    in_port_t       sin6_port;     /* port number */
-    uint32_t        sin6_flowinfo; /* IPv6 flow information */
-    struct in6_addr sin6_addr;     /* IPv6 address */
-    uint32_t        sin6_scope_id; /* Scope ID (new in 2.4) */
-};
-int main(int argc, char *argv[]) {
-    int port, sockFD;
 
-    sockFD = socket(AF_INET6, SOCK_STREAM, 0);
+int main(int argc, char *argv[]) {
+    int port, sockFD, sucessRate;
+    struct sockaddr_in6 defaultConfig = {
+        AF_INET6,
+        htons(44325),
+        0,
+        in6addr_any,
+        0     
+    };
+    sockFD = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
     if (sockFD < 0) {
         cout << "failed" << endl;
     }
     cout << "sucess" << endl;
+
+    sucessRate = bind(sockFD, (struct sockaddr*) &defaultConfig, sizeof(defaultConfig));
+    
+    if (sucessRate < 0) {
+        cout << "failed bind" << endl;
+    }
+    cout << "sucess bind" << endl;
+
+    listen(sockFD, 50);
+
+    close(sockFD);
+
     return 0;
 }
